a63640df68f7c42759bfa5c8c96ba9075a4a0c6d
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-05-27 08:49:42 +0100

    Split out functions

--- server/src/hub.rs
+++ server/src/hub.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
+use common::messages::Message;
 use tokio::sync::mpsc::{Receiver, Sender};
 
 use crate::events::{ClientEvent, ServerEvent};
@@ -21,20 +22,24 @@ impl Hub {
         loop {
             let msg = server_rx.recv().await.unwrap();
             match msg {
-                ClientEvent::OnConnect(addr, server_tx) => {
-                    println!("client connected from {addr}");
-                    self.clients.insert(addr, server_tx);
-                },
-                ClientEvent::OnMessage(addr, msg) => {
-                    println!("Received message from {}: \"{:?}\"", &addr, &msg);
-                    let event = Arc::new(ServerEvent::OnMessage(msg));
-                    for (client_addr, tx) in &self.clients {
-                        if *client_addr != addr {
-                            tx.send(event.clone()).await.unwrap();
-                        }
-                    };
-                },
+                ClientEvent::OnConnect(addr, server_tx) => self.handle_connect(addr, server_tx),
+                ClientEvent::OnMessage(addr, msg) => self.handle_message(addr, msg).await,
             }
         }    
+    }
+
+    fn handle_connect(&mut self, addr: SocketAddr, server_tx: Sender<Arc<ServerEvent>>) {
+        println!("client connected from {addr}");
+        self.clients.insert(addr, server_tx);
+    }
+
+    async fn handle_message(&mut self, addr: SocketAddr, msg: Message) {
+        println!("Received message from {addr}: \"{msg:?}\"");
+        let event = Arc::new(ServerEvent::OnMessage(msg));
+        for (client_addr, tx) in &self.clients {
+            if *client_addr != addr {
+                tx.send(event.clone()).await.unwrap();
+            }
+        };
     }
 }
