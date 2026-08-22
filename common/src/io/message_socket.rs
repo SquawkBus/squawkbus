759daf84d09f1a275dce65a632efe90073079c42
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-08-22 07:24:10 +0100

    comments

--- common/src/io/message_socket.rs
+++ common/src/io/message_socket.rs
@@ -29,15 +29,19 @@ where
     T: AsyncRead + AsyncWrite + Unpin + Send,
 {
     async fn read(&mut self) -> io::Result<Message> {
+        // Read the frame length.
         let mut len_buf = [0_u8; 4];
         self.reader.read_exact(&mut len_buf).await?;
         let len = u32::from_be_bytes(len_buf);
 
         log::trace!("Reading a frame of {len} bytes.");
 
+        // Read the frame with the known length.
         let mut buf: Vec<u8> = vec![0; len as usize];
         self.reader.read_exact(&mut buf).await?;
         let mut cursor = Cursor::new(buf);
+
+        // Parse the message.
         Message::deserialize(&mut cursor)
     }
 
@@ -45,6 +49,7 @@ where
         let len = message.size();
         let mut cursor: Cursor<Vec<u8>> = Cursor::new(Vec::with_capacity(4 + len));
 
+        // Write the length of the frame into the cursor, then write the message.
         (len as u32).serialize(&mut cursor)?;
         message.serialize(&mut cursor)?;
 
